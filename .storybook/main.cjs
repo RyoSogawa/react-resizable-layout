920d09fe0ca338a0fab1385e9ea3c35ba8bd924d
RyoSogawa <koashimitekara.1122@gmail.com>
2022-03-05 22:34:53 +0900

    remove storybook addon-interaction and testing-library

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -6,7 +6,6 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
     '@storybook/addon-storysource',
   ],
   framework: '@storybook/react',
