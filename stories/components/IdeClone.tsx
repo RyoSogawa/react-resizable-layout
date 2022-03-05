01c08264cc7e526f2c97c7535f2108f3050ae0be
RyoSogawa <koashimitekara.1122@gmail.com>
2022-03-05 21:51:29 +0900

    use isDragging state in IDE Clone

--- stories/components/IdeClone.tsx
+++ stories/components/IdeClone.tsx
@@ -1,36 +1,37 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './IdeClone.css'
 import SampleDragBar from './SampleDragBar'
 import { useResizable } from '../../src'
 
 const IdeClone = (): JSX.Element => {
-  const [draggingTarget, setDraggingTarget] = useState<
-    'terminal' | 'file' | 'plugin' | null
-  >(null)
-
-  const { position: terminalH, dragBarProps: terminalDragBarProps } =
-    useResizable({
-      axis: 'y',
-      initial: 150,
-      min: 50,
-      reverse: true,
-      onResizeStart: () => setDraggingTarget('terminal'),
-      onResizeEnd: () => setDraggingTarget(null),
-    })
-  const { position: fileW, dragBarProps: fileDragBarProps } = useResizable({
+  const {
+    isDragging: isTerminalDragging,
+    position: terminalH,
+    dragBarProps: terminalDragBarProps,
+  } = useResizable({
+    axis: 'y',
+    initial: 150,
+    min: 50,
+    reverse: true,
+  })
+  const {
+    isDragging: isFileDragging,
+    position: fileW,
+    dragBarProps: fileDragBarProps,
+  } = useResizable({
     axis: 'x',
     initial: 250,
     min: 50,
-    onResizeStart: () => setDraggingTarget('file'),
-    onResizeEnd: () => setDraggingTarget(null),
   })
-  const { position: pluginW, dragBarProps: pluginDragBarProps } = useResizable({
+  const {
+    isDragging: isPluginDragging,
+    position: pluginW,
+    dragBarProps: pluginDragBarProps,
+  } = useResizable({
     axis: 'x',
     initial: 200,
     min: 50,
     reverse: true,
-    onResizeStart: () => setDraggingTarget('plugin'),
-    onResizeEnd: () => setDraggingTarget(null),
   })
 
   const cn = (...args: any[]) => args.filter(Boolean).join(' ')
@@ -39,10 +40,7 @@ const IdeClone = (): JSX.Element => {
     <div className={'flex flex-column h-screen bg-dark font-mono color-white'}>
       <div className={'flex grow'}>
         <div
-          className={cn(
-            'shrink-0 contents',
-            draggingTarget === 'file' && 'dragging'
-          )}
+          className={cn('shrink-0 contents', isFileDragging && 'dragging')}
           style={{ width: fileW }}
         >
           File Tree
@@ -52,10 +50,7 @@ const IdeClone = (): JSX.Element => {
           <div className={'grow bg-darker contents'}>Editor</div>
           <SampleDragBar {...pluginDragBarProps} />
           <div
-            className={cn(
-              'shrink-0 contents',
-              draggingTarget === 'plugin' && 'dragging'
-            )}
+            className={cn('shrink-0 contents', isPluginDragging && 'dragging')}
             style={{ width: pluginW }}
           >
             Plugin
@@ -66,7 +61,7 @@ const IdeClone = (): JSX.Element => {
       <div
         className={cn(
           'shrink-0 bg-darker contents',
-          draggingTarget === 'terminal' && 'dragging'
+          isTerminalDragging && 'dragging'
         )}
         style={{ height: terminalH }}
       >
