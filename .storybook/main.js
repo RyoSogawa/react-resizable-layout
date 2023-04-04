b567f620bddb091e78d8bf4c697ffe65019f696b
RyoSogawa <koashimitekara.1122@gmail.com>
2023-04-04 23:02:27 +0900

    chore: upgrade storybook to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,16 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-storysource'],
-  framework: '@storybook/react',
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-storysource',
+    '@storybook/addon-mdx-gfm',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
 };
