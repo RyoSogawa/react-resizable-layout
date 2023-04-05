86b5691ecff022d867081a9c2ba177c94a3a8bf7
RyoSogawa <koashimitekara.1122@gmail.com>
2023-04-05 23:09:15 +0900

    chore: storybook-webpack to vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,10 @@
+import { mergeConfig } from 'vite';
+
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-storysource'],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
+  framework: '@storybook/react-vite',
+  async viteFinal(config) {
+    return mergeConfig(config);
   },
 };
