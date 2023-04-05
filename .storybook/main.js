7ad3d9c23391abc7b8d4779d20b9c70acdb12cf0
RyoSogawa <koashimitekara.1122@gmail.com>
2023-04-05 22:37:28 +0900

    chore: remove @storybook/addon-mdx-gfm and autodocs option

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,8 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-storysource',
-    '@storybook/addon-mdx-gfm',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-storysource'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
-  },
-  docs: {
-    autodocs: true,
   },
 };
