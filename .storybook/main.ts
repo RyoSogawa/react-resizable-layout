43e1358d04063e59fc718cd8a20e6b6464a8870d
RyoSogawa <koashimitekara.1122@gmail.com>
2023-04-05 23:15:07 +0900

    chore: storybook config files to typescript

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,14 @@
 import { mergeConfig } from 'vite';
 
-module.exports = {
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-storysource'],
   framework: '@storybook/react-vite',
   async viteFinal(config) {
-    return mergeConfig(config);
+    return mergeConfig(config, {});
   },
 };
+
+export default config;
