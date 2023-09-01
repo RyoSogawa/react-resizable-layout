d633d59062bdd050622ba36f0beef9f90064e76a
RyoSogawa <koashimitekara.1122@gmail.com>
2023-09-01 14:40:06 +0900

    feat:update callback story with new args

--- stories/Callback.stories.tsx
+++ stories/Callback.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import SampleBox from './components/SampleBox';
 import SampleSeparator from './components/SampleSeparator';
@@ -16,34 +16,29 @@ export const Callback: StoryObj<typeof Resizable> = {
     axis: 'x',
     initial: 200,
   },
-  render: (props) => {
-    const [date, setDate] = useState<Date | null>(null);
-
-    return (
-      <Resizable
-        {...props}
-        onResizeStart={() => setDate(new Date())}
-        onResizeEnd={() => alert(`You dragged!`)}
-      >
-        {({ position: x, separatorProps }) => (
-          <div style={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
-            <SampleBox
-              id="left-block"
-              theme="blue"
-              width={x}
-              size={x}
-              text={date && `dragging from ${date.toISOString()}`}
-            />
-            <SampleSeparator id="splitter" {...separatorProps} />
-            <SampleBox
-              id="right-block"
-              theme="red"
-              width={`calc(100% - ${x}px)`}
-              text={date && `dragging from ${date.toISOString()}`}
-            />
-          </div>
-        )}
-      </Resizable>
-    );
-  },
+  render: (props) => (
+    <Resizable
+      {...props}
+      onResizeStart={(args) => {
+        console.table(args);
+      }}
+      onResizeEnd={(args) => {
+        console.table(args);
+      }}
+    >
+      {({ position: x, separatorProps }) => (
+        <div style={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
+          <SampleBox
+            id="left-block"
+            theme="blue"
+            width={x}
+            size={x}
+            text="You can see the callback arguments in the log"
+          />
+          <SampleSeparator id="splitter" {...separatorProps} />
+          <SampleBox id="right-block" theme="red" width={`calc(100% - ${x}px)`} />
+        </div>
+      )}
+    </Resizable>
+  ),
 };
